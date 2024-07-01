908c0340d47b2eed02cb5a3b67f2df3403f1d112
aditi <meaditisingh22@gmail.com>
2024-07-02 00:10:45 +0530

    first commit

---
+++ TODOapp/todo/tasks/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+
+'''
+Pointer
+needs to be done to see the app in admin panel
+
+
+
+'''
+
+
+from .models import *
+admin.site.register(Task)
---
+++ TODOapp/todo/tasks/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+'''Pointer
+Here a model is created that is a table in db and fields are defined where default for complete is set , and created is added automatically
+also __str__ is used so that when 
+tasks = Task.objects.all() is called it doesnt show as  Task object (1) but as the title of it 
+
+we can use below if i want to show according to status of complete
+    def __str__(self) :
+        return f"{self.complete}"
+
+'''
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    
+    
+    def __str__(self) :
+        return f"{self.title}"
